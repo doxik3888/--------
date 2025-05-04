5093416bd7eadb44073f98f44980c6ac418339ca
doxik3888 <153354996+doxik3888@users.noreply.github.com>
2025-05-04 17:29:17 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
 
-const sensitivity = 0.4
+const sensitivity = 0.1
 
 document.addEventListener("mousemove", (e) =>{
     let dx = (e.clientX - window.innerWidth / 2)* sensitivity
@@ -8,16 +8,16 @@ document.addEventListener("mousemove", (e) =>{
 
     gsap.to(".layers_container", {
         duration: 1.5,
-        x:dx,
-        y:dy,
-        rotationX: dy/10,
-        rotationY: dx/10,
+        x:-dx,
+        y:-dy,
+        rotationX: dy/15,
+        rotationY: dx/15,
         ease: "power2.out",
     })
     gsap.to(".head-text", {
         duration: 1.5,
-        x: -dx/2,
-        y: -dy/2,
+        x: dx/3,
+        y: dy/3,
         ease: "power2.out",
     })
 
@@ -42,3 +42,23 @@ gsap.utils.toArray("section").forEach(section => {
         }}
     )
 });
+
+
+function initGallery(){
+    let gallery = document.querySelector(".gallery")
+    let galleryItem = document.querySelectorAll(".gallery-item")
+    gallery.style.setProperty("--total-item", galleryItem.length)
+
+    gallery.addEventListener("click", (event) =>{
+        let clicked = event.target.closest(".gallery-item")
+        if (!clicked || clicked.classList.contains("active")) return
+
+        galleryItem.forEach((item) => {
+            item.classList.remove("active")
+        })
+        clicked.classList.add("active")
+    })
+
+}
+
+document.addEventListener("DOMContentLoaded",initGallery)
